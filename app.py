5c6fee4b40ec8543e96477e0d96dea2f54620d89
Pre Seo Tools Key <preseotoolskey@gmail.com>
2025-05-22 03:28:24 +0530

    Fix: bind Flask app to 0.0.0.0 and use Render PORT environment variable

--- app.py
+++ app.py
@@ -14,4 +14,6 @@ def get_keywords():
     return jsonify(suggestions)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
